3115c376e8bb221af9dce6970a039db961b6fd5f
ICEYSELF <icey@icey.tech>
2024-11-09 20:27:16 +0800

    部分重构代码并支持灵感

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const memoryBook = []
 const compressedChatLog = []
 const chatLog = []
 
+const invertedSystemLog = []
+
 const timestamp = Date.now() - 0
 const logFile = `./logs/${timestamp}.log`
 let jsonLogFile = `./logs/${timestamp}.json`
@@ -30,6 +32,30 @@ const writeLog = async (message, level) => {
 const removeLastConsoleLine = () => {
    process.stdout.moveCursor(0, -1)
    process.stdout.clearLine()
+}
+
+const invertedChatLog = () => {
+   const ret = []
+   for (const log of chatLog) {
+      if (log.role === 'user') {
+         ret.push({
+            role: 'assistant',
+            name: log.name,
+            content: log.content
+         })
+      }
+      else if (log.role !== '旁白') {
+         ret.push({
+            role: 'user',
+            name: log.name,
+            content: log.content
+         })
+      }
+      else {
+         ret.push(log)
+      }
+   }
+   return ret
 }
 
 const applicationStart = async () => {
@@ -110,6 +136,254 @@ const applicationStart = async () => {
    await chatMain(apiKey, characterFile, selfFile, character, self)
 }
 
+const setupSystemLog = (character, self, selfName) => {
+   // 在系统级提示中记录角色的设置
+   systemLog.push({
+      role: 'system',
+      name: character.name,
+      content: character.settings + (character.hiddenSettings ?? '')
+   })
+
+   // 在系统级提示中记录自设的设置
+   systemLog.push({
+      role: 'user_system',
+      name: selfName,
+      content: self ? self.settings : ''
+   })
+
+   // 添加旁白角色
+   systemLog.push({
+      role: 'system',
+      name: '旁白',
+      content: '会客观总结当前情形的旁白'
+   })
+}
+
+const setupInvertedSystemLog = (character, self, selfName) => {
+   // 反转系统级提示中的用户和系统角色
+   invertedSystemLog.push({
+      role: 'system',
+      nam: selfName,
+      content: self ? self.settings : ''
+   })
+
+   invertedSystemLog.push({
+      role: 'user_system',
+      name: character.name,
+      content: character.settings + (character.hiddenSettings ?? '')
+   })
+
+   invertedSystemLog.push({
+      role: 'system',
+      name: '旁白',
+      content: '会客观总结当前情形的旁白'
+   })
+}
+
+const acceptUserInput = async (selfName, character) => {
+   let commentatorMode = false
+   let modifyMode = false
+   const input = await prompt({
+      type: 'text',
+      name: 'value',
+      message: () => commentatorMode ? '旁白' : (modifyMode ? '重写' : selfName),
+      format: value => {
+         commentatorMode = value.trim().startsWith('/')
+         modifyMode = value.trim().startsWith('!')
+         return value
+      },
+      validate: value => {
+         value = value.trim()
+         if (value.length === 0) {
+            return '对话内容不能为空'
+         }
+
+         if (value === '?' || value.startsWith('!')) {
+            if (chatLog.length === 1
+                || chatLog[chatLog.length - 1].role !== 'assistant'
+                || chatLog[chatLog.length - 1].name !== character.name) {
+               return '不能重写开场白或者非对话角色的发言'
+            }
+
+            if (value === '!') {
+               return '必须提供适当的重写文本'
+            }
+         }
+
+         if (value === '/') {
+            return '必须提供适当的旁白文本'
+         }
+         return true
+      }
+   })
+   removeLastConsoleLine()
+
+   if (!input.value) {
+      return undefined
+   }
+   return input.value.trim()
+}
+
+const compressMemoryCommand = async command => {
+   if (chatLog.length <= 1) {
+      console.warn(chalk.yellowBright('没有可以压缩的对话记录'))
+      return
+   }
+
+   const parts = command.split(' ').map(part => part.trim()).filter(part => part.length > 0)
+   const chatLogLength = chatLog.length
+   let toBeCompressedLength
+   if (parts.length === 1) {
+      toBeCompressedLength = chatLogLength / 2
+   }
+   else if (parts.length === 2) {
+      if (parts[1] === 'all') {
+         toBeCompressedLength = chatLogLength
+      }
+      else {
+         const numArg = parseFloat(parts[1])
+         if (isNaN(numArg)) {
+            console.warn(chalk.yellowBright(`compress: 参数 ${parts[1]} 无效`))
+         }
+
+         if (numArg > 0.0 && numArg <= 1.0) {
+            toBeCompressedLength = Math.round(chatLogLength * numArg)
+            if (toBeCompressedLength === 0) {
+               console.warn(chalk.yellowBright('compress: 若按比例压缩，至少需要压缩一条对话记录'))
+               return
+            }
+         }
+         else if (numArg < 0) {
+            toBeCompressedLength = Math.round(chatLogLength + numArg)
+            if (toBeCompressedLength < 1) {
+               console.warn(chalk.yellowBright(`compress: 若按数量压缩，至少需要压缩一条对话记录 (目前共有 ${chatLogLength} 条未压缩的记录)`))
+               return
+            }
+         }
+         else {
+            console.warn(chalk.yellowBright(`compress: 参数 ${parts[1]} 无效`))
+            return
+         }
+      }
+   }
+   else {
+      console.warn(chalk.yellowBright('compress: 参数过多'))
+      return
+   }
+
+   const toBeCompressedLog = chatLog.splice(0, toBeCompressedLength)
+   const compressed = await compressMemory(apiKey, toBeCompressedLog)
+   memoryBook.push({
+      role: 'assistant',
+      name: '旁白',
+      content: compressed.content
+   })
+   compressedChatLog.push(...toBeCompressedLog)
+   return
+}
+
+const regenerateCommand = async (apiKey, character, model) => {
+   const resp = await postJSON(
+      'https://api.minimax.chat/v1/text/chatcompletion_v2',
+      {
+         'Content-Type': 'application/json',
+         'Authorization': `Bearer ${apiKey}`
+      },
+      {
+         model,
+         messages: [
+            ...systemLog,
+            ...memoryBook,
+            ...chatLog.slice(0, -1)
+         ],
+         n: 3
+      }
+   )
+   if (resp.base_resp.status_code !== 0) {
+      console.error(chalk.redBright(chalk.bold('API 错误: ') + `(${resp.base_resp.status_code}) ${resp.base_resp.status_msg}`))
+      await writeLog(`API 错误: (${resp.base_resp.status_code}) ${resp.base_resp.status_msg}`, 'ERROR')
+      return
+   }
+   writeLog(`使用 token 额度: ${resp.usage.total_tokens}`, 'INFO')
+
+   const choices = resp.choices.map((choice, idx) => {
+      return { name: choice.message.content, value: `${idx}` }
+   })
+   const choice = (await prompt({
+      type: 'select',
+      name: 'value',
+      message: '选择一个重写',
+      choices: [
+         ...choices,
+         { name: '无', value: -1 }
+      ],
+      result() {
+         return this.focused.value
+      }
+   })).value
+
+   removeLastConsoleLine()
+   if (choice !== -1) {
+      const choiceText = choices[choice].name
+      chatLog[chatLog.length - 1].content = choiceText
+      removeLastConsoleLine()
+      console.info(chalk.bold(`${character.name}: `) + choiceText)
+      await writeLog(`已修改对话内容 ${character.name}: ${choiceText}`, 'CHAT')
+   }
+}
+
+const inspirationCommand = async (apiKey, selfName, model) => {
+   const resp = await postJSON(
+      'https://api.minimax.chat/v1/text/chatcompletion_v2',
+      {
+         'Content-Type': 'application/json',
+         'Authorization': `Bearer ${apiKey}`
+      },
+      {
+         model,
+         messages: [
+            ...invertedSystemLog,
+            ...memoryBook,
+            ...invertedChatLog()
+         ],
+         n: 3
+      }
+   )
+   if (resp.base_resp.status_code !== 0) {
+      console.error(chalk.redBright(chalk.bold('API 错误: ') + `(${resp.base_resp.status_code}) ${resp.base_resp.status_msg}`))
+      await writeLog(`API 错误: (${resp.base_resp.status_code}) ${resp.base_resp.status_msg}`, 'ERROR')
+      return
+   }
+   writeLog(`使用 token 额度: ${resp.usage.total_tokens}`, 'INFO')
+
+   const choices = resp.choices.map((choice, idx) => {
+      return { name: choice.message.content, value: `${idx}` }
+   })
+   const choice = (await prompt({
+      type: 'select',
+      name: 'value',
+      message: '选择一个灵感',
+      choices: [
+         ...choices,
+         { name: '无', value: -1 }
+      ],
+      result() {
+         return this.focused.value
+      }
+   })).value
+   removeLastConsoleLine()
+
+   if (choice !== -1) {
+      const choiceText = choices[choice].name
+      chatLog.push({ role: 'user', name: selfName, content: choiceText })
+      console.info(chalk.bold(`${selfName}: `) + choiceText)
+      await writeLog(`${selfName}: ${choiceText}`, 'CHAT')
+      return true
+   }
+
+   return false
+}
+
 const chatMain = async (apiKey, characterFile, selfFile, character, self) => {
    // 日志记录
    await writeLog(`选择了角色: ${characterFile}`, 'INFO')
@@ -131,26 +405,8 @@ const chatMain = async (apiKey, characterFile, selfFile, character, self) => {
    // 选择正确的用户名
    const selfName = self ? self.name : '用户'
 
-   // 在系统级提示中记录角色的设置
-   systemLog.push({
-      role: 'system',
-      name: character.name,
-      content: character.settings + (character.hiddenSettings ?? '')
-   })
-
-   // 在系统级提示中记录自设的设置
-   systemLog.push({
-      role: 'user_system',
-      name: selfName,
-      content: self ? self.settings : ''
-   })
-
-   // 添加旁白角色
-   systemLog.push({
-      role: 'system',
-      name: '旁白',
-      content: '会客观总结当前情形的旁白'
-   })
+   setupSystemLog(character, self, selfName)
+   setupInvertedSystemLog(character, self, selfName)
 
    // 添加开场白到对话记录中
    if (chatLog.length === 0) {
@@ -178,48 +434,8 @@ const chatMain = async (apiKey, characterFile, selfFile, character, self) => {
    }
 
    while (true) {
-      let commentatorMode = false
-      let modifyMode = false
-      const input = await prompt({
-         type: 'text',
-         name: 'value',
-         message: () => commentatorMode ? '旁白' : (modifyMode ? '重写' : selfName),
-         format: value => {
-            commentatorMode = value.trim().startsWith('/')
-            modifyMode = value.trim().startsWith('!')
-            return value
-         },
-         validate: value => {
-            value = value.trim()
-            if (value.length === 0) {
-               return '对话内容不能为空'
-            }
+      const command = await acceptUserInput(selfName, character)
 
-            if (value === '?' || value.startsWith('!')) {
-               if (chatLog.length === 1
-                   || chatLog[chatLog.length - 1].role !== 'assistant'
-                   || chatLog[chatLog.length - 1].name !== character.name) {
-                  return '不能重写开场白或者非对话角色的发言'
-               }
-
-               if (value === '!') {
-                  return '必须提供适当的重写文本'
-               }
-            }
-
-            if (value === '/') {
-               return '必须提供适当的旁白文本'
-            }
-            return true
-         }
-      })
-      removeLastConsoleLine()
-
-      if (input.value === undefined) {
-         break
-      }
-
-      const command = input.value.trim()
       if (command === 'exit' || command === 'quit') {
          break
       }
@@ -235,60 +451,7 @@ const chatMain = async (apiKey, characterFile, selfFile, character, self) => {
       }
 
       if (command.startsWith('compress')) {
-         if (chatLog.length <= 1) {
-            console.warn(chalk.yellowBright('没有可以压缩的对话记录'))
-            continue
-         }
-
-         const parts = command.split(' ').map(part => part.trim()).filter(part => part.length > 0)
-         const chatLogLength = chatLog.length
-         let toBeCompressedLength
-         if (parts.length === 1) {
-            toBeCompressedLength = chatLogLength / 2
-         }
-         else if (parts.length === 2) {
-            if (parts[1] === 'all') {
-               toBeCompressedLength = chatLogLength
-            }
-            else {
-               const numArg = parseFloat(parts[1])
-               if (isNaN(numArg)) {
-                  console.warn(chalk.yellowBright(`compress: 参数 ${parts[1]} 无效`))
-               }
-
-               if (numArg > 0.0 && numArg <= 1.0) {
-                  toBeCompressedLength = Math.round(chatLogLength * numArg)
-                  if (toBeCompressedLength === 0) {
-                     console.warn(chalk.yellowBright('compress: 若按比例压缩，至少需要压缩一条对话记录'))
-                     continue
-                  }
-               }
-               else if (numArg < 0) {
-                  toBeCompressedLength = Math.round(chatLogLength + numArg)
-                  if (toBeCompressedLength < 1) {
-                     console.warn(chalk.yellowBright(`compress: 若按数量压缩，至少需要压缩一条对话记录 (目前共有 ${chatLogLength} 条未压缩的记录)`))
-                     continue
-                  }
-               }
-               else {
-                  console.warn(chalk.yellowBright(`compress: 参数 ${parts[1]} 无效`))
-                  continue
-               }
-            }
-         }
-         else {
-            console.warn(chalk.yellowBright('compress: 参数过多'))
-            continue
-         }
-
-         const toBeCompressedLog = chatLog.splice(0, toBeCompressedLength)
-         const compressed = await compressMemory(apiKey, toBeCompressedLog)
-         memoryBook.push({
-            role: 'assistant',
-            name: '旁白',
-            content: compressed.content
-         })
-         compressedChatLog.push(...toBeCompressedLog)
+         await compressMemoryCommand(command)
          continue
       }
 
@@ -303,57 +466,21 @@ const chatMain = async (apiKey, characterFile, selfFile, character, self) => {
       }
 
       if (command === '?') {
-         const resp = await postJSON(
-            'https://api.minimax.chat/v1/text/chatcompletion_v2',
-            {
-               'Content-Type': 'application/json',
-               'Authorization': `Bearer ${apiKey}`
-            },
-            {
-               model,
-               messages: [
-                  ...systemLog,
-                  ...memoryBook,
-                  ...chatLog.slice(0, -1)
-               ],
-               n: 3
-            }
-         )
-         if (resp.base_resp.status_code !== 0) {
-            console.error(chalk.redBright(chalk.bold('API 错误: ') + `(${resp.base_resp.status_code}) ${resp.base_resp.status_msg}`))
-            await writeLog(`API 错误: (${resp.base_resp.status_code}) ${resp.base_resp.status_msg}`, 'ERROR')
-            continue
-         }
-         writeLog(`使用 token 额度: ${resp.usage.total_tokens}`, 'INFO')
-
-         const choices = resp.choices.map((choice, idx) => {
-            return { name: choice.message.content, value: `${idx}` }
-         })
-         const choice = (await prompt({
-            type: 'select',
-            name: 'value',
-            message: '选择一个重写',
-            choices: [
-               ...choices,
-               { name: '无', value: -1 }
-            ],
-            result() {
-               return this.focused.value
-            }
-         })).value
-         removeLastConsoleLine()
-         if (choice !== -1) {
-            const choiceText = choices[choice].name
-            chatLog[chatLog.length - 1].content = choiceText
-            removeLastConsoleLine()
-            console.info(chalk.bold(`${character.name}: `) + choiceText)
-            await writeLog(`已修改对话内容 ${character.name}: ${choiceText}`, 'CHAT')
-         }
+         await regenerateCommand(apiKey, character, model)
          continue
       }
 
       if (command === '~') {
          // 什么都不做，跳过此轮用户输入
+      }
+      else if (command === ':') {
+         // 如果用户要求灵感
+         const usingInspiration = await inspirationCommand(apiKey, selfName, model)
+         // 如果用户没有采用灵感，则进入下一轮用户输入
+         if (!usingInspiration) {
+            continue
+         }
+         // 否则开始生成 AI 回复
       }
       else if (command.startsWith('/')) {
          const content = command.substring(1).trim()
